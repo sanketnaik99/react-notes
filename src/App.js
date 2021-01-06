94b26c75e19a27a00a41394edc0506d6a187a324
Sanket Naik <sanketnaik99@gmail.com>
2021-01-06 19:12:06 +0530

    App.js Error Fix ✔

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ import {
 
 // const M = require("materialize-css");
 
-// const App = () => {
-//   useEffect(() => {
-//     // M.Sidenav.init();
-//   }, []);
+const App = () => {
+  // useEffect(() => {
+  //   // M.Sidenav.init();
+  // }, []);
 
   return (
     <div className="App">
