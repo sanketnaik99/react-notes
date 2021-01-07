c39c78be17e9e869028da2f9af700de74d729fe2
Sanket Naik <sanketnaik99@gmail.com>
2021-01-07 13:27:28 +0530

    ⚙ Service Worker configuration Updated

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { combineReducers, createStore } from "redux";
 import notesReducer from "./store/reducers/notesReducer";
 import { Provider } from "react-redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const rootReducer = combineReducers({ notes: notesReducer });
 
@@ -25,7 +25,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
